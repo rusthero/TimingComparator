3559263203e73bda6362221255db0d59f6a27bbc
rusthero <rusthero@proton.me>
2023-02-11 08:20:04 +0300

    Mixin WorldRenderer to increase accuracy and add message cooldown

--- src/main/java/dev/rusthero/timingcomparator/mixin/WorldRendererMixin.java
+++ src/main/java/dev/rusthero/timingcomparator/mixin/WorldRendererMixin.java
@@ -2,11 +2,9 @@ package dev.rusthero.timingcomparator.mixin;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.TrapdoorBlock;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.world.ClientWorld;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Final;
@@ -19,46 +17,52 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import static java.lang.String.format;
 
 @Environment(EnvType.CLIENT)
-@Mixin(ClientWorld.class)
-public abstract class ClientWorldMixin {
+@Mixin(WorldRenderer.class)
+public abstract class WorldRendererMixin {
     private static Long lastActivate = 0L;
     private static Long lastDeactivate = 0L;
+    private static Long lastActivateMessage = 0L;
+    private static Long lastDeactivateMessage = 0L;
 
     @Shadow
     @Final
     private MinecraftClient client;
 
-    @Shadow
-    @Final
-    private ClientWorld.Properties clientWorldProperties;
-
     @Inject(
             at = @At("HEAD"),
-            method = "updateListeners(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/block/BlockState;I)V"
+            method = "processWorldEvent(Lnet/minecraft/entity/player/PlayerEntity;ILnet/minecraft/util/math/BlockPos;I)V"
     )
-    public void onTrapdoorUpdate(BlockPos pos, BlockState oldState, BlockState newState, int flags, CallbackInfo ci) {
+    public void onTrapdoorUpdate(PlayerEntity source, int eventId, BlockPos pos, int data, CallbackInfo ci) {
         if (client.player == null) return;
-        if (oldState.getBlock() != newState.getBlock() || newState.getBlock() != Blocks.IRON_TRAPDOOR) return;
-
-        boolean oldPowered = oldState.get(TrapdoorBlock.POWERED);
-        boolean powered = newState.get(TrapdoorBlock.POWERED);
-        if (oldPowered == powered) return;
+        if (eventId != 1036 && eventId != 1037) return;
 
         String status;
         final long now = System.currentTimeMillis();
         long passedTime;
+        boolean sendMessage = false;
 
-        if (powered) {
+        if (eventId == 1037) {
             status = "§aactivated";
             passedTime = now - lastActivate;
             lastActivate = now;
+
+            if (passedTime < 50 && now - lastActivateMessage >= 50) {
+                lastActivateMessage = now;
+                sendMessage = true;
+            }
         } else {
             status = "§cdeactivated";
             passedTime = now - lastDeactivate;
             lastDeactivate = now;
+
+            if (passedTime < 50 && now - lastDeactivateMessage >= 50) {
+                lastDeactivateMessage = now;
+                sendMessage = true;
+            }
         }
 
-        if (passedTime < 50)
+        if (sendMessage) {
             client.player.sendMessage(Text.of(format("§eTrapdoors %s§e at the same time.", status)), false);
+        }
     }
 }
